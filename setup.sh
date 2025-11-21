71654292bb9ebfbebd7a273537ad6d3c5d060acd
Lionel Henne <lionelhenne@gmail.com>
2025-11-21 19:29:11 +0100

    feat(ssh): add backup and include directive for SSH config - Backup existing ~/.ssh/config to ~/.ssh/config.bak if it exists - Add 'Include ~/.ssh/config.local' to the SSH config if not already present - Ensure proper permissions (600) for SSH config files - Fix indentation in SSH config file for compatibility

--- setup.sh
+++ setup.sh
@@ -217,8 +217,10 @@ module_core() {
         chmod 700 "$HOME/.ssh"
         cat <<EOF > "$HOME/.ssh/config"
 Include ~/.ssh/config.local
+
 Host *
-    IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    IdentityAgent ~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
 Host github.com
     HostName github.com
     User git
